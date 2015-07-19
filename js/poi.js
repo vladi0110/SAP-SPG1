2f916acc404c7c93ce689c43e582907aed0d7705
plamengv <caldur@abv.bg>
2015-07-20 01:06:05 +0300

    Cleanup
    Cleanup Javascript

--- js/poi.js
+++ js/poi.js
@@ -2,3 +2,14 @@ function changeDivContent() {
 	document.getElementById("content").innerHTML = Date();
 	document.getElementById("content").className = "content2";
 }
+
+function initialize() {
+	var mapOptions = {
+		center: { lat: 42.65703, lng: 23.35661},
+		zoom: 16
+	};
+	var map = new google.maps.Map(document.getElementById('map-canvas'),
+		mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
