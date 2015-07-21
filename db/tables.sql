dd76c547d932344b8a034891d9374a88f78688e0
plamengv <caldur@abv.bg>
2015-07-21 21:20:00 +0300

    DB
    Database

---
+++ db/tables.sql
@@ -0,0 +1,24 @@
+USE `map`;
+
+DROP TABLE IF EXISTS `poi`;
+
+CREATE TABLE `poi` (
+	`id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`type` ENUM ('Ключар', 'Аптека', 'Железария', 'Магазин', 'Копирен център', 'АТМ') NOT NULL,
+	`description` VARCHAR(255) NULL DEFAULT NULL,
+	`latitude` FLOAT (10, 6) NOT NULL,
+	`longitude` FLOAT (10, 6) NOT NULL,
+	`image` VARCHAR(255) NULL DEFAULT NULL,
+	`address` VARCHAR(255) NULL DEFAULT NULL,
+	`working hours` VARCHAR(255) NULL DEFAULT NULL,
+	`advertizer` BIT(1) NOT NULL DEFAULT '0',
+	`account` ENUM ('Normal', 'Silver', 'Gold', 'Platinum') NOT NULL DEFAULT 'Normal',
+	`phone` VARCHAR(255) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=INNODB;
+
+INSERT INTO poi(name, type, latitude, longitude, ) VALUES
+	(''),
+	(''),
+	('');
