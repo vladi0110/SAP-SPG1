cef08d9493d2d825c518d1c533dfcd5fd2528a6a
Vladislav Marinov <vladi_im@abv.bg>
2015-07-22 13:58:00 +0300

    travel

---
+++ travel/assets/js/routes.js
@@ -0,0 +1,49 @@
+var foundationRoutes = [{
+    "name": "settings",
+    "url": "/settings",
+    "animationIn": "slideFromLeft",
+    "animationOut": "slideFromLeft",
+    "path": "templates/settings.html"
+}, {
+    "name": "keymakers",
+    "url": "/keymakers",
+    "animationIn": "hingeInFromMiddleY bounceInOut",
+    "animationOut": "zoomOut bounceInOut",
+    "path": "templates/keymakers.html"
+}, {
+    "name": "copycenters",
+    "url": "/copycenters",
+    "animationIn": "hingeInFromMiddleY bounceInOut",
+    "animationOut": "zoomOut bounceInOut",
+    "path": "templates/copycenters.html"
+}, {
+    "name": "ironmongeries",
+    "url": "/ironmongeries",
+    "animationIn": "hingeInFromMiddleY bounceInOut",
+    "animationOut": "zoomOut bounceInOut",
+    "path": "templates/ironmongeries.html"
+}, {
+    "name": "atms",
+    "url": "/atms",
+    "animationIn": "hingeInFromMiddleY bounceInOut",
+    "animationOut": "zoomOut bounceInOut",
+    "path": "templates/atms.html"
+}, {
+    "name": "shops",
+    "url": "/shops",
+    "animationIn": "hingeInFromMiddleY bounceInOut",
+    "animationOut": "zoomOut bounceInOut",
+    "path": "templates/shops.html"
+}, {
+    "name": "pharmacies",
+    "url": "/pharmacies",
+    "animationIn": "hingeInFromMiddleY bounceInOut",
+    "animationOut": "zoomOut bounceInOut",
+    "path": "templates/pharmacies.html"
+}, {
+    "name": "listings",
+    "url": "/",
+    "animationIn": "hingeInFromMiddleY bounceInOut",
+    "animationOut": "zoomOut bounceInOut",
+    "path": "templates/home.html"
+}];
